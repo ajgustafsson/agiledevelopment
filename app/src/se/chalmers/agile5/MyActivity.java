4e8bade1ee800193e982e8b4f52476118892f706
unknown <johannes.sjoberg@live.se>
2014-04-01 14:42:51 +0200

    Added a Actionbar with a few empty activities

--- app/src/se/chalmers/agile5/MyActivity.java
+++ app/src/se/chalmers/agile5/MyActivity.java
@@ -2,6 +2,8 @@ package se.chalmers.agile5;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MyActivity extends Activity {
     /**
@@ -12,4 +14,13 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_items, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
 }
