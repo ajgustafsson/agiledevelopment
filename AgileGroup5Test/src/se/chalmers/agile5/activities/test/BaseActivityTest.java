19f9509e86e7f381725596e0c3a5b85a1f834b2e
freein <freein@student.chalmers.se>
2014-04-08 11:03:28 +0200

    Add working test project

---
+++ AgileGroup5Test/src/se/chalmers/agile5/activities/test/BaseActivityTest.java
@@ -0,0 +1,41 @@
+package se.chalmers.agile5.activities.test;
+
+import junit.framework.Assert;
+import se.chalmers.agile5.activities.BaseActivity;
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+
+public class BaseActivityTest extends ActivityUnitTestCase<BaseActivity> {
+
+	public BaseActivityTest(){
+		super(BaseActivity.class);
+	}
+	
+	@Override
+	/**
+	 * This method is called before everything else
+	 */
+	public void setUp() {
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+	}
+	/**
+	 * Called last.
+	 * Use it to clean up.
+	 */
+	@Override
+	public void tearDown(){
+		
+	}
+	
+	/**
+	 * Another useful convention is to add the method testPreconditions() to your test class. Use this method to test that the application under test is initialized correctly. If this test fails, you know that that the initial conditions were in error. When this happens, further test results are suspect, regardless of whether or not the tests succeeded.
+	 */
+	public void testPreconditions(){
+	}
+	
+	@MediumTest
+	public void test1(){
+		Assert.assertTrue(true);
+	}
+}
