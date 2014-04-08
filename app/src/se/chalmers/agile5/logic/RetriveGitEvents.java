124573e4ec30a70446cb71889a986a40fa029702
andergu <anders@ajgustafsson.se>
2014-04-08 10:30:21 +0200

    Merged with Development branch

--- app/src/se/chalmers/agile5/logic/RetriveGitEvents.java
+++ app/src/se/chalmers/agile5/logic/RetriveGitEvents.java
@@ -1,5 +1,24 @@
 package se.chalmers.agile5.logic;
 
+import java.io.IOException;
+
+import org.eclipse.egit.github.core.service.CommitService;
+
+import se.chalmers.agile5.entities.GitDataHandler;
+
 public class RetriveGitEvents {
 
+	public RetriveGitEvents() {
+		
+		GitDataHandler handler = new GitDataHandler();
+		CommitService commitService = new CommitService(handler.getGitClient());
+		try {
+			System.out.println(commitService.getCommits(handler.getCurrentGitRepo()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
