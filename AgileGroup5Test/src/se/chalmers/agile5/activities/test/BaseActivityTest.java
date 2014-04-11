138379e1bbb41824013bc322103f306721bb4327
freein <freein@student.chalmers.se>
2014-04-11 14:11:12 +0200

    Added unit tests

--- AgileGroup5Test/src/se/chalmers/agile5/activities/test/BaseActivityTest.java
+++ AgileGroup5Test/src/se/chalmers/agile5/activities/test/BaseActivityTest.java
@@ -1,17 +1,17 @@
 package se.chalmers.agile5.activities.test;
 
 import junit.framework.Assert;
+import se.chalmers.agile5.R;
 import se.chalmers.agile5.activities.BaseActivity;
 import android.content.Intent;
 import android.test.ActivityUnitTestCase;
 import android.test.suitebuilder.annotation.MediumTest;
+import android.view.Menu;
 
 public class BaseActivityTest extends ActivityUnitTestCase<BaseActivity> {
 
 	// Activity to test
 	BaseActivity baseActivity;
-	
-	
 	
 	public BaseActivityTest(){
 		super(BaseActivity.class);
@@ -21,16 +21,21 @@ public class BaseActivityTest extends ActivityUnitTestCase<BaseActivity> {
 	/**
 	 * This method is called before everything else
 	 */
-	public void setUp() {
-		Intent intent = new Intent(Intent.ACTION_MAIN);
+	public void setUp() throws Exception {
+		super.setUp();
+		final Intent intent = new Intent(getInstrumentation().getTargetContext(), BaseActivity.class);
+		startActivity(intent,null,null);
+		baseActivity = (BaseActivity) getActivity();
+		
 	}
 	/**
 	 * Called last.
 	 * Use it to clean up.
 	 */
 	@Override
-	public void tearDown(){
-		
+	public void tearDown() throws Exception{
+		super.tearDown();
+		baseActivity.finish();
 	}
 	
 	/**
@@ -40,7 +45,14 @@ public class BaseActivityTest extends ActivityUnitTestCase<BaseActivity> {
 	}
 	
 	@MediumTest
-	public void test1(){
-		Assert.assertTrue(true);
+	public void testBackButton(){
+		//Testing if the activity is properly closed
+		baseActivity.onBackPressed();
+		Assert.assertTrue(isFinishCalled());
+	}
+	
+	@MediumTest
+	public void testMenuCalls(){
+		Menu menu = (Menu) baseActivity.findViewById(R.menu.actionbar_items);
 	}
 }
