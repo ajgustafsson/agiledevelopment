587e45a804d0c6c1db7ec81831f42e45001feda7
Johannes Vestlund <iceman@westlundarna.se>
2014-05-06 10:33:30 +0200

    Expanded roadmap entry.

--- app/src/se/chalmers/agile5/entities/RoadMapEntry.java
+++ app/src/se/chalmers/agile5/entities/RoadMapEntry.java
@@ -1,21 +1,30 @@
 package se.chalmers.agile5.entities;
 
+import java.sql.Timestamp;
+
 /**
- * Created by armin on 4/29/14.
+ * Logical representation of task entry in Roadmap.
+ * 
+ * Created by Armin on 4/29/14 and cowritten with Johannes Vestlund.
  */
 public class RoadMapEntry {
 
     private String title;
-
     private String description;
-
+    private EntryType type;
+    private Timestamp creationDate;
+    private boolean done;
+    
     public RoadMapEntry(String title) {
-        this(title, "");
+        this(title, "", EntryType.CUSTOM);
     }
 
-    public RoadMapEntry(String title, String description) {
+    public RoadMapEntry(String title, String description, EntryType type) {
         this.title = title;
         this.description = description;
+        this.type = type;
+        this.creationDate = new Timestamp(System.currentTimeMillis());
+        this.done = false;
     }
 
     public String getTitle() {
@@ -24,6 +33,18 @@ public class RoadMapEntry {
 
     public String getDescription() {
         return description;
+    }
+    
+    public Timestamp getCreationDate(){
+    	return creationDate;
+    }
+    
+    public void setDone(){
+    	done = true;
+    }
+    
+    public boolean isDone(){
+    	return done;
     }
 
     @Override
